87b9c1d1ef6283a20fe6b3ce8c516955d742f142
Pietro Saccardi <lizardm4@gmail.com>
2021-06-05 00:50:48 +0200

    Add basic rgb and hsv helper classes

--- src/neopixel_color.cpp
+++ src/neopixel_color.cpp
@@ -4,12 +4,8 @@
 
 #include "neopixel_color.hpp"
 #include <cmath>
-#include <algorithm>
 
 namespace neo {
-
-    static constexpr float pi_thirds = M_PI / 3.f;
-    static constexpr float two_pi = 2.f * M_PI;
 
     namespace {
         std::uint8_t f2b(float f) {
@@ -21,7 +17,8 @@ namespace neo {
         }
     }
 
-    std::array<float, 3> rgb_to_hsv(std::uint8_t r, std::uint8_t g, std::uint8_t b) {
+    hsv rgb::to_hsv() const {
+        static constexpr auto hue_scale = 1.f / 6.f;
         const std::uint8_t value = std::max(r, std::max(g, b));
         const std::uint8_t min_c = std::min(r, std::min(g, b));
         const std::uint8_t chroma = value - min_c;
@@ -30,47 +27,91 @@ namespace neo {
             if (chroma == 0) {
                 return 0.f;
             } else if (value == r) {
-                return pi_thirds * float(g - b) / float(chroma);
+                return hue_scale * float(g - b) / float(chroma);
             } else if (value == g) {
-                return pi_thirds * (2.f + float(b - r) / float(chroma));
+                return hue_scale * (2.f + float(b - r) / float(chroma));
             } else if (value == b) {
-                return pi_thirds * (4.f + float(r - g) / float(chroma));
+                return hue_scale * (4.f + float(r - g) / float(chroma));
             }
             return 0.f;
         }();
 
         const float saturation = (value == 0 ? 0.f : float(chroma) / float(value));
 
-        return {std::fmod(hue + two_pi, two_pi), saturation, float(value) / 255.f};
+        return {std::fmod(hue + 1.f, 1.f), saturation, float(value) / 255.f};
     }
 
-
-    std::array<std::uint8_t, 3> hsv_to_rgb(float h, float s, float v) {
-        s = std::clamp(s, 0.f, 1.f);
-        v = std::clamp(v, 0.f, 1.f);
-        if (s < std::numeric_limits<float>::epsilon()) {
-            const std::uint8_t gray = f2b(v);
+    rgb hsv::to_rgb() const {
+        const float s_ = std::clamp(s, 0.f, 1.f);
+        const float v_ = std::clamp(v, 0.f, 1.f);
+        if (s_ < std::numeric_limits<float>::epsilon()) {
+            const std::uint8_t gray = f2b(v_);
             return {gray, gray, gray};
         }
         // Remap in 0...6
-        h = std::fmod(h / pi_thirds, 6.f);
-        const auto hue_block = unsigned(std::floor(h));
-        const float m = v * (1.f - s);
-        const float x = v * (1.f - s * (hue_block % 2 == 0 ? 1.f - h + float(hue_block) : h - float(hue_block)));
+        const float h_ = 6.f * std::clamp(h - std::floor(h), 0.f, 1.f);
+        const auto hue_floor = std::floor(h_);
+        const auto hue_block = unsigned(hue_floor) % 6;
+        const float m = v_ * (1.f - s_);
+        const float x = v_ * (1.f - s_ * (hue_block % 2 == 0 ? 1.f - h_ + hue_floor : h_ - hue_floor));
         switch (hue_block) {
             case 0:
-                return f2b({v, x, m});
+                return f2b({v_, x, m});
             case 1:
-                return f2b({x, v, m});
+                return f2b({x, v_, m});
             case 2:
-                return f2b({m, v, x});
+                return f2b({m, v_, x});
             case 3:
-                return f2b({m, x, v});
+                return f2b({m, x, v_});
             case 4:
-                return f2b({x, m, v});
+                return f2b({x, m, v_});
             default:
-                return f2b({v, m, x});
+                return f2b({v_, m, x});
         }
+    }
+
+    hsv &hsv::clamp() {
+        h = std::clamp(h - std::floor(h), 0.f, 1.f);
+        s = std::clamp(s, 0.f, 1.f);
+        v = std::clamp(v, 0.f, 1.f);
+        return *this;
+    }
+
+    hsv &hsv::update(maybe_update<float> h_, maybe_update<float> s_, maybe_update<float> v_) {
+        h_.set(h);
+        s_.set(s);
+        v_.set(v);
+        return *this;
+    }
+
+    rgb &rgb::update(maybe_update<std::uint8_t> r_, maybe_update<std::uint8_t> g_, maybe_update<std::uint8_t> b_) {
+        r_.set(r);
+        g_.set(g);
+        b_.set(b);
+        return *this;
+    }
+
+    rgb &rgb::shift(maybe_update<signed> dr, maybe_update<signed> dg, maybe_update<signed> db) {
+        dr.add(r);
+        dg.add(g);
+        db.add(b);
+        return *this;
+    }
+
+    hsv &hsv::shift(maybe_update<float> dh, maybe_update<float> ds, maybe_update<float> dv) {
+        dh.add(h);
+        ds.add(s);
+        dv.add(v);
+        clamp();
+        return *this;
+    }
+
+    rgb &rgb::blend(const rgb &target, float factor) {
+        factor = std::clamp(factor, 0.f, 1.f);
+        r = f2b(float(r) * (1.f - factor) + float(target.r) * factor);
+        g = f2b(float(g) * (1.f - factor) + float(target.g) * factor);
+        b = f2b(float(b) * (1.f - factor) + float(target.b) * factor);
+        return *this;
     }
 
 }
--- src/neopixel_color.hpp
+++ src/neopixel_color.hpp
@@ -7,15 +7,173 @@
 
 #include <cstdint>
 #include <array>
+#include <algorithm>
+
+namespace neo {
+    struct hsv;
+
+    struct keep_t {
+    };
+
+    static constexpr keep_t keep{};
+
+    template<class T>
+    struct maybe_update {
+        const bool update;
+        T value;
+
+        maybe_update(keep_t);
+
+        maybe_update(T value_);
+
+        void set(T &target) const;
+
+        template<class U>
+        void add(U &target) const;
+    };
+
+    struct rgb {
+        std::uint8_t r = 0;
+        std::uint8_t g = 0;
+        std::uint8_t b = 0;
+
+        rgb() = default;
+
+        inline rgb(std::uint32_t rgb_);
+
+        inline rgb(std::array<std::uint8_t, 3> rgb_);
+
+        inline rgb(std::uint8_t r_, std::uint8_t g_, std::uint8_t b_);
+
+        rgb &update(maybe_update<std::uint8_t> r_, maybe_update<std::uint8_t> g_, maybe_update<std::uint8_t> b_);
+        [[nodiscard]] inline rgb update(maybe_update<std::uint8_t> r_, maybe_update<std::uint8_t> g_, maybe_update<std::uint8_t> b_) const;
+
+        rgb &shift(maybe_update<signed> dr, maybe_update<signed> dg, maybe_update<signed> db);
+        [[nodiscard]] inline rgb shift(maybe_update<signed> dr, maybe_update<signed> dg, maybe_update<signed> db) const;
+
+        inline rgb &shift(rgb const &delta, bool negate = false);
+        [[nodiscard]] inline rgb shift(rgb const &delta, bool negate = false) const;
+
+        rgb &blend(rgb const &target, float factor);
+        [[nodiscard]] inline rgb blend(rgb const &target, float factor) const;
+
+        [[nodiscard]] hsv to_hsv() const;
+
+    };
+
+    struct hsv {
+        float h = 0.f;
+        float s = 0.f;
+        float v = 0.f;
+
+        hsv() = default;
+
+        inline hsv(std::array<float, 3> hsv_);
+
+        inline hsv(float h_, float s_, float v_);
+
+        hsv &clamp();
+
+        hsv &update(maybe_update<float> h_, maybe_update<float> s_, maybe_update<float> v_);
+        [[nodiscard]] inline hsv update(maybe_update<float> h_, maybe_update<float> s_, maybe_update<float> v_) const;
+
+        hsv &shift(maybe_update<float> dh, maybe_update<float> ds, maybe_update<float> dv);
+        [[nodiscard]] inline hsv shift(maybe_update<float> dh, maybe_update<float> ds, maybe_update<float> dv) const;
+
+        inline hsv &shift(hsv const &delta, bool negate = false);
+        [[nodiscard]] inline hsv shift(hsv const &delta, bool negate = false) const;
+
+        [[nodiscard]] rgb to_rgb() const;
+    };
+
+}
 
 namespace neo {
 
-    /**
-     * @return Hue, saturation, value in range [0, 2PI], [0, 1], [0, 1].
-     */
-    std::array<float, 3> rgb_to_hsv(std::uint8_t r, std::uint8_t g, std::uint8_t b);
+    rgb::rgb(std::uint8_t r_, std::uint8_t g_, std::uint8_t b_) : r{r_}, g{g_}, b{b_} {}
 
-    std::array<std::uint8_t, 3> hsv_to_rgb(float h, float s, float v);
+    rgb::rgb(std::array<std::uint8_t, 3> rgb_) : rgb{rgb_[0], rgb_[1], rgb_[2]} {}
+
+    rgb::rgb(std::uint32_t rgb_) : rgb{
+            std::uint8_t(0xff & (rgb_ >> 16)),
+            std::uint8_t(0xff & (rgb_ >> 8)),
+            std::uint8_t(0xff & rgb_)} {}
+
+
+    hsv::hsv(float h_, float s_, float v_) : h{h_}, s{s_}, v{v_} {}
+
+    hsv::hsv(std::array<float, 3> hsv_) : hsv{hsv_[0], hsv_[1], hsv_[2]} {}
+
+
+    template<class T>
+    maybe_update<T>::maybe_update(keep_t) : update{false}, value{} {}
+
+    template<class T>
+    maybe_update<T>::maybe_update(T value_) : update{true}, value{value_} {}
+
+    template<class T>
+    void maybe_update<T>::set(T &target) const {
+        if (update) {
+            target = value;
+        }
+    }
+
+    template<class T>
+    template<class U>
+    void maybe_update<T>::add(U &target) const {
+        if (update) {
+            if constexpr (not std::is_signed_v<U> and std::is_signed_v<T>) {
+                target = U(std::clamp(T(target + value),
+                                      T(std::numeric_limits<U>::lowest()),
+                                      T(std::numeric_limits<U>::max())));
+            } else {
+                target += value;
+            }
+        }
+    }
+
+    rgb &rgb::shift(const rgb &delta, bool negate) {
+        return negate ? shift(-delta.r, -delta.g, -delta.b) : shift(delta.r, delta.g, delta.b);
+    }
+
+    hsv &hsv::shift(hsv const &delta, bool negate) {
+        return negate ? shift(-delta.h, -delta.s, -delta.v) : shift(delta.h, delta.s, delta.v);
+    }
+
+    rgb rgb::update(maybe_update<std::uint8_t> r_, maybe_update<std::uint8_t> g_, maybe_update<std::uint8_t> b_) const {
+        rgb retval = *this;
+        return retval.update(r_, g_, b_);
+    }
+
+    rgb rgb::shift(maybe_update<signed> dr, maybe_update<signed> dg, maybe_update<signed> db) const {
+        rgb retval = *this;
+        return retval.shift(dr, dg, db);
+    }
+
+    rgb rgb::shift(rgb const &delta, bool negate) const {
+        rgb retval = *this;
+        return retval.shift(delta, negate);
+    }
+
+    hsv hsv::update(maybe_update<float> h_, maybe_update<float> s_, maybe_update<float> v_) const {
+        hsv retval = *this;
+        return retval.update(h_, s_, v_);
+    }
+
+    hsv hsv::shift(maybe_update<float> dh, maybe_update<float> ds, maybe_update<float> dv) const {
+        hsv retval = *this;
+        return retval.shift(dh, ds, dv);
+    }
+
+    hsv hsv::shift(hsv const &delta, bool negate) const {
+        hsv retval = *this;
+        return retval.shift(delta, negate);
+    }
+
+    rgb rgb::blend(rgb const &target, float factor) const {
+        rgb retval = *this;
+        return retval.blend(target, factor);
+    }
 
 }
 
