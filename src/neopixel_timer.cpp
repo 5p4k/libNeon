3c5d90cc6468571a9c704b15e21ad2073bb99528
Pietro Saccardi <lizardm4@gmail.com>
2021-06-09 01:53:33 +0200

    Implement timer that reports the elapsed time

--- src/neopixel_timer.cpp
+++ src/neopixel_timer.cpp
@@ -7,15 +7,19 @@
 
 
 namespace neo {
+    namespace {
+        using namespace std::chrono_literals;
+    }
+
     static constexpr auto timer_timeout = pdMS_TO_TICKS(100);
 
-    timer::timer() : _name{}, _elapsed{}, _last_fire{std::numeric_limits<TickType_t>::max()}, _timer{nullptr} {}
+    generic_timer::generic_timer() : _name{}, _timer{nullptr}, _cbk{nullptr} {}
 
-    timer &timer::operator=(timer &&t) noexcept {
+    generic_timer &generic_timer::operator=(generic_timer &&t) noexcept {
         if (t.is_active() or is_active()) {
-            ESP_LOGW("TIMER", "Moving active timer, let us hope it does not fire now");
+            ESP_LOGW("TIMER", "Moving active generic_timer, let us hope it does not fire now");
         }
-        // Change the timer id to point to this
+        // Change the generic_timer id to point to this
         if (t._timer) {
             vTimerSetTimerID(t._timer, this);
         }
@@ -23,94 +27,139 @@ namespace neo {
             vTimerSetTimerID(_timer, &t);
         }
         std::swap(t._name, _name);
-        std::swap(t._elapsed, _elapsed);
         std::swap(t._timer, _timer);
+        std::swap(t._cbk, _cbk);
         return *this;
     }
 
-    timer::timer(timer &&t) noexcept : timer{} {
+    generic_timer::generic_timer(generic_timer &&t) noexcept: generic_timer{} {
         *this = std::move(t);
     }
 
-    timer::timer(std::string name_, std::chrono::milliseconds period, bool start) :
-        _name{std::move(name_)},
-        _elapsed{0},
-        _last_fire{std::numeric_limits<TickType_t>::max()},
-        _timer{nullptr}
-    {
+    generic_timer::generic_timer(std::string name_, std::chrono::milliseconds period, std::function<void(generic_timer &)> cbk,
+                                 bool start) :
+            _name{std::move(name_)},
+            _timer{nullptr},
+            _cbk{std::move(cbk)} {
         _timer = xTimerCreate(name().c_str(), pdMS_TO_TICKS(period.count()), pdTRUE, this, &callback);
         if (_timer == nullptr) {
-            ESP_LOGE("TIMER", "Could not create timer \"%s\".", name().c_str());
+            ESP_LOGE("TIMER", "Could not create generic_timer \"%s\".", name().c_str());
         } else if (start) {
             this->start();
         }
     }
 
-    void timer::start() {
+    void generic_timer::start() {
         if (_timer != nullptr) {
             if (xTimerStart(_timer, timer_timeout) != pdPASS) {
-                ESP_LOGE("TIMER", "Could not start timer \"%s\".", name().c_str());
+                ESP_LOGE("TIMER", "Could not start generic_timer \"%s\".", name().c_str());
             }
         }
     }
 
-    void timer::callback(TimerHandle_t hdl) {
-        if (auto *instance = reinterpret_cast<timer *>(pvTimerGetTimerID(hdl)); instance != nullptr) {
-            instance->callback();
+    void generic_timer::invoke_callback() {
+        if (_cbk != nullptr) {
+            _cbk(*this);
+        }
+    }
+
+    void generic_timer::callback(TimerHandle_t hdl) {
+        if (auto *instance = reinterpret_cast<generic_timer *>(pvTimerGetTimerID(hdl)); instance != nullptr) {
+            instance->invoke_callback();
         } else {
-            ESP_LOGE("TIMER", "Null timer instance.");
+            ESP_LOGE("TIMER", "Null generic_timer instance.");
         }
     }
 
-    void timer::callback() {
-        // Update the last fired event and add the extra elapsed time
-        const auto now = xTaskGetTickCount();
-        if (_last_fire != std::numeric_limits<TickType_t>::max()) {
-
-        }
-        _last_fire = xTaskGetTickCount();
-#warning Use chrono instead
-    }
-
-    void timer::stop() {
+    void generic_timer::stop() {
         if (_timer != nullptr) {
             if (xTimerStop(_timer, timer_timeout) != pdPASS) {
-                ESP_LOGE("TIMER", "Could not stop timer \"%s\".", name().c_str());
+                ESP_LOGE("TIMER", "Could not stop generic_timer \"%s\".", name().c_str());
             }
         }
     }
 
-    void timer::reset() {
+    void generic_timer::reset() {
         if (_timer != nullptr) {
             if (xTimerReset(_timer, timer_timeout) != pdPASS) {
-                ESP_LOGE("TIMER", "Could not reset timer \"%s\".", name().c_str());
+                ESP_LOGE("TIMER", "Could not reset generic_timer \"%s\".", name().c_str());
             }
         }
     }
 
-    void timer::reset(std::chrono::milliseconds new_period) {
+    void generic_timer::reset(std::chrono::milliseconds new_period) {
         if (_timer != nullptr) {
-            const bool active = is_active();
             if (xTimerChangePeriod(_timer, pdMS_TO_TICKS(new_period.count()), timer_timeout) != pdPASS) {
-                ESP_LOGE("TIMER", "Could not change period of timer \"%s\".", name().c_str());
+                ESP_LOGE("TIMER", "Could not change period of generic_timer \"%s\".", name().c_str());
             }
         }
     }
 
-    std::chrono::milliseconds timer::period() const {
+    std::chrono::milliseconds generic_timer::period() const {
         if (_timer) {
             return std::chrono::milliseconds{pdTICKS_TO_MS(xTimerGetPeriod(_timer))};
         }
         return {};
     }
 
-    bool timer::is_active() const {
+    bool generic_timer::is_active() const {
         return _timer != nullptr and xTimerIsTimerActive(_timer);
     }
 
-    timer::~timer() {
+    generic_timer::~generic_timer() {
         if (_timer) {
             xTimerDelete(_timer, pdMS_TO_TICKS(timer_timeout));
         }
     }
+
+    steady_timer::steady_timer() : generic_timer{}, _last_start{}, _previous_laps_duration{} {}
+
+    void steady_timer::elapsed_callback(generic_timer &gen_timer) {
+
+
+    }
+
+    steady_timer::steady_timer(std::string name_, std::chrono::milliseconds period,
+                               std::function<void(std::chrono::milliseconds)> cbk, bool start) :
+            generic_timer{std::move(name_), period,
+                          [elapsed_cbk = std::move(cbk)](generic_timer &gt) {
+                              elapsed_cbk(reinterpret_cast<steady_timer *>(&gt)->elapsed());
+                          }, start},
+            _last_start{std::chrono::steady_clock::now()},
+            _previous_laps_duration{0ms} {}
+
+    void steady_timer::start() {
+        if (not is_active()) {
+            _last_start = std::chrono::steady_clock::now();
+            generic_timer::start();
+        }
+    }
+
+    std::chrono::milliseconds steady_timer::elapsed_since_last_start() const {
+        return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - _last_start);
+    }
+
+    std::chrono::milliseconds steady_timer::elapsed() const {
+        return is_active() ? _previous_laps_duration + elapsed_since_last_start() : _previous_laps_duration;
+    }
+
+    void steady_timer::stop() {
+        if (is_active()) {
+            _previous_laps_duration = elapsed();
+            generic_timer::stop();
+        }
+    }
+
+    void steady_timer::reset() {
+        _previous_laps_duration = 0ms;
+        _last_start = std::chrono::steady_clock::now();
+        generic_timer::reset();
+    }
+
+    void steady_timer::reset(std::chrono::milliseconds new_period) {
+        _previous_laps_duration = 0ms;
+        _last_start = std::chrono::steady_clock::now();
+        generic_timer::reset(new_period);
+    }
+
 }
--- src/neopixel_timer.hpp
+++ src/neopixel_timer.hpp
@@ -9,44 +9,105 @@
 #include <freertos/timers.h>
 #include <string>
 #include <chrono>
+#include <functional>
 
 namespace neo {
-    class timer {
+
+    class generic_timer {
         std::string _name;
-        std::chrono::milliseconds _elapsed;
-        TickType_t _last_fire;
         TimerHandle_t _timer;
+        std::function<void(generic_timer &)> _cbk;
 
         static void callback(TimerHandle_t hdl);
-        void callback();
+
     public:
-        timer();
-        timer(std::string name_, std::chrono::milliseconds period, bool start = false);
-        timer(timer const &) = delete;
-        timer &operator=(timer const &) = delete;
-        timer &operator=(timer &&t) noexcept;
-        timer(timer &&t) noexcept;
-        ~timer();
+        generic_timer();
+
+        generic_timer(std::string name_, std::chrono::milliseconds period, std::function<void(generic_timer &)> cbk, bool start = false);
+
+        generic_timer(generic_timer const &) = delete;
+
+        generic_timer &operator=(generic_timer const &) = delete;
+
+        generic_timer &operator=(generic_timer &&t) noexcept;
+
+        generic_timer(generic_timer &&t) noexcept;
+
+        virtual ~generic_timer();
 
         [[nodiscard]] inline std::string const &name() const;
+
         [[nodiscard]] std::chrono::milliseconds period() const;
+
         [[nodiscard]] bool is_active() const;
 
-        void start();
-        void stop();
-        void reset();
-        void reset(std::chrono::milliseconds new_period);
+        void invoke_callback();
+
+        virtual void start();
+
+        virtual void stop();
+
+        /**
+         * @note Will @ref start the timer.
+         */
+        virtual void reset();
+
+        /**
+         * @note Will @ref start the timer.
+         */
+        virtual void reset(std::chrono::milliseconds new_period);
 
         inline explicit operator bool() const;
     };
+
+    class steady_timer : private generic_timer {
+        std::chrono::time_point<std::chrono::steady_clock> _last_start;
+        std::chrono::milliseconds _previous_laps_duration;
+
+        static void elapsed_callback(generic_timer &gen_timer);
+        [[nodiscard]] std::chrono::milliseconds elapsed_since_last_start() const;
+    public:
+        
+        using generic_timer::operator bool;
+        using generic_timer::is_active;
+        using generic_timer::name;
+        using generic_timer::period;
+
+        steady_timer();
+
+        steady_timer(std::string name_, std::chrono::milliseconds period, std::function<void(std::chrono::milliseconds)> cbk, bool start = false);
+
+        steady_timer(steady_timer const &) = delete;
+        steady_timer(steady_timer &&) noexcept = default;
+
+        steady_timer &operator=(steady_timer const &) = delete;
+        steady_timer &operator=(steady_timer &&) noexcept = default;
+
+        [[nodiscard]] std::chrono::milliseconds elapsed() const;
+
+        void start() override;
+
+        void stop() override;
+
+        /**
+         * @note Will @ref start the timer.
+         */
+        void reset() override;
+
+        /**
+         * @note Will @ref start the timer.
+         */
+        void reset(std::chrono::milliseconds new_period) override;
+    };
+
 }
 
 namespace neo {
-    const std::string & timer::name() const {
+    const std::string &generic_timer::name() const {
         return _name;
     }
 
-    timer::operator bool() const {
+    generic_timer::operator bool() const {
         return is_active();
     }
 }
