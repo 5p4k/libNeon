7089d857f859cbc09c0a0f4eacedf40d2e0516e7
Pietro Saccardi <lizardm4@gmail.com>
2023-08-21 12:09:39 +0200

    Update gradient fx

--- libneon/examples/gradient_fx.cpp
+++ libneon/examples/gradient_fx.cpp
@@ -1,5 +1,6 @@
 #include <neo/alarm.hpp>
 #include <neo/encoder.hpp>
+ #include <neo/fx.hpp>
 #include <neo/gradient.hpp>
 
 static constexpr gpio_num_t strip_gpio_pin = GPIO_NUM_13;
@@ -11,15 +12,11 @@ using namespace neo::literals;
 extern "C" void app_main() {
     neo::led_encoder encoder{neo::encoding::ws2812b, neo::make_rmt_config(strip_gpio_pin)};
 
-    const auto rainbow = neo::gradient_make_uniform_from_colors(
-            {0xff0000_rgb, 0xffff00_rgb, 0x00ff00_rgb, 0x00ffff_rgb, 0x0000ff_rgb, 0xff00ff_rgb, 0xff0000_rgb});
+    const auto rainbow_fx = neo::wrap(neo::gradient_fx{
+            {0xff0000_rgb, 0xffff00_rgb, 0x00ff00_rgb, 0x00ffff_rgb, 0x0000ff_rgb, 0xff00ff_rgb, 0xff0000_rgb},
+            5s});
 
-    auto animate = [&, buffer = std::vector<neo::srgb>{strip_num_leds}](neo::alarm &a) mutable {
-        neo::gradient_sample(std::begin(rainbow), std::end(rainbow), buffer.size(), std::begin(buffer), a.cycle_time(4s));
-        ESP_ERROR_CHECK(encoder.transmit(std::begin(buffer), std::end(buffer), neo::srgb_linear_channel_extractor()));
-    };
-
-    neo::alarm alarm{30_fps, animate};
+    neo::alarm alarm{30_fps, rainbow_fx->make_callback(encoder, strip_num_leds)};
     alarm.start();
 
     vTaskSuspend(nullptr);
